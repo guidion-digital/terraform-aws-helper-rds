597fac73def43acb4682e7749cf524164563980d
Afraz Ahmadzadeh <afrazo@proton.me>
2025-02-27 21:20:17 +0100

    feat: Output patched together mysql_proxy_user_arn

--- outputs.tf
+++ outputs.tf
@@ -30,7 +30,7 @@ output "mysql_replica_instance_name" {
 
 output "mysql_proxy_arn" {
   description = "The Amazon Resource Name (ARN) for the proxy"
-  value       = try(module.mysql_rds_proxy[0].arn, null)
+  value       = try(module.mysql_rds_proxy[0].proxy_arn, null)
 }
 
 output "mysql_proxy_endpoint" {
@@ -46,4 +46,18 @@ output "mysql_proxy_endpoints" {
 output "mysql_db_name" {
   description = "Name of the database"
   value       = local.db_name
+}
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+locals {
+  proxy_arn_split = try(split(":", module.mysql_rds_proxy[0].proxy_arn), null)
+  proxy_real_id   = try(local.proxy_arn_split[length(local.proxy_arn_split) - 1], null)
+  proxy_user_arn  = try("arn:aws:rds-db:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:dbuser:${local.proxy_real_id}/*", null)
+}
+
+output "mysql_proxy_user_arn" {
+  description = "ARN for the proxy user"
+  value       = local.proxy_user_arn
 }
