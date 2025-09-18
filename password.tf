c4b5737cab101f684d133f441227ceb330667b20
Afraz Ahmadzadeh <afrazo@proton.me>
2025-09-18 15:19:58 +0200

    No app-lambda module usage
    That module is not public yet, and the functionality it's used for is a WIP anyway. So in order to fix the example, we'll just comment it out

--- password.tf
+++ password.tf
@@ -56,59 +56,59 @@ resource "aws_secretsmanager_secret_version" "rds_password" {
 #
 # Peiced together from here:
 # https://aws.amazon.com/blogs/security/rotate-amazon-rds-database-credentials-automatically-with-aws-secrets-manager/
-module "rds_mysql_password_rotator_lambda" {
-  count = var.password_rotation_days != null ? 1 : 0
-
-  source  = "app.terraform.io/guidion/app-lambda/aws"
-  version = "0.0.10"
-
-  application_name            = var.application_name
-  stage                       = var.stage
-  project                     = var.project
-  grafana_promtail_lambda_arn = var.grafana_promtail_lambda_arn
-  build_in_docker             = var.build_lambdas_in_docker
-
-  lambdas = {
-    "rds-mysql-password-rotator" = {
-      description = "RDS MySQL Password Rotator"
-      source_dir  = "${path.module}/lambdas/rds-mysql-secret-rotator"
-      runtime     = "python3.11"
-      handler     = "main.handler"
-      lambda_role = var.rotator_lambda_role_name
-
-      rotator_for = {
-        "secretsmanager" = {
-          path = local.secret_name
-        }
-      }
-
-      vpc_subnet_ids         = var.subnet_ids
-      vpc_security_group_ids = local.vpc_security_group_ids
-
-      environment_variables = {
-        EXCLUDE_CHARACTERS         = "/@\"'"
-        EXCLUDE_LOWERCASE          = "false"
-        EXCLUDE_NUMBERS            = "false"
-        EXCLUDE_PUNCTUATION        = "false"
-        EXCLUDE_UPPERCASE          = "false"
-        PASSWORD_LENGTH            = "32"
-        REQUIRE_EACH_INCLUDED_TYPE = "true"
-        SECRETS_MANAGER_ENDPOINT   = "https://secretsmanager.eu-central-1.amazonaws.com"
-      }
-    }
-  }
-}
-
-resource "aws_secretsmanager_secret_rotation" "this" {
-  count = var.password_rotation_days != null ? 1 : 0
-
-  secret_id           = module.rds_password_secret.arns[local.secret_name]
-  rotation_lambda_arn = module.rds_mysql_password_rotator_lambda[0].arns["rds-mysql-password-rotator"]
-  rotate_immediately  = var.rotate_password_immediately
-
-  rotation_rules {
-    automatically_after_days = var.password_rotation_days
-    duration                 = var.password_rotation_duration
-    schedule_expression      = var.password_rotation_schedule_expression
-  }
-}
+# module "rds_mysql_password_rotator_lambda" {
+#   count = var.password_rotation_days != null ? 1 : 0
+#
+#   source  = "app.terraform.io/guidion/app-lambda/aws"
+#   version = "0.0.10"
+#
+#   application_name            = var.application_name
+#   stage                       = var.stage
+#   project                     = var.project
+#   grafana_promtail_lambda_arn = var.grafana_promtail_lambda_arn
+#   build_in_docker             = var.build_lambdas_in_docker
+#
+#   lambdas = {
+#     "rds-mysql-password-rotator" = {
+#       description = "RDS MySQL Password Rotator"
+#       source_dir  = "${path.module}/lambdas/rds-mysql-secret-rotator"
+#       runtime     = "python3.11"
+#       handler     = "main.handler"
+#       lambda_role = var.rotator_lambda_role_name
+#
+#       rotator_for = {
+#         "secretsmanager" = {
+#           path = local.secret_name
+#         }
+#       }
+#
+#       vpc_subnet_ids         = var.subnet_ids
+#       vpc_security_group_ids = local.vpc_security_group_ids
+#
+#       environment_variables = {
+#         EXCLUDE_CHARACTERS         = "/@\"'"
+#         EXCLUDE_LOWERCASE          = "false"
+#         EXCLUDE_NUMBERS            = "false"
+#         EXCLUDE_PUNCTUATION        = "false"
+#         EXCLUDE_UPPERCASE          = "false"
+#         PASSWORD_LENGTH            = "32"
+#         REQUIRE_EACH_INCLUDED_TYPE = "true"
+#         SECRETS_MANAGER_ENDPOINT   = "https://secretsmanager.eu-central-1.amazonaws.com"
+#       }
+#     }
+#   }
+# }
+#
+# resource "aws_secretsmanager_secret_rotation" "this" {
+#   count = var.password_rotation_days != null ? 1 : 0
+#
+#   secret_id           = module.rds_password_secret.arns[local.secret_name]
+#   rotation_lambda_arn = module.rds_mysql_password_rotator_lambda[0].arns["rds-mysql-password-rotator"]
+#   rotate_immediately  = var.rotate_password_immediately
+#
+#   rotation_rules {
+#     automatically_after_days = var.password_rotation_days
+#     duration                 = var.password_rotation_duration
+#     schedule_expression      = var.password_rotation_schedule_expression
+#   }
+# }
